d75518ce42b43a2b5ac1c943a87884f55d6ac786
JasonYCHuang <jasonych99@gmail.com>
2021-11-02 10:15:23 +0800

    create counter example

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,10 @@
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import Counter from './counter';
 
-const Hello = () => {
+const EntryPoint = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <Counter />
     </div>
   );
 };
@@ -43,7 +13,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={EntryPoint} />
       </Switch>
     </Router>
   );
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,11 @@
-import { render } from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from './reducers/store'
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
