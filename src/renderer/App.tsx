6dcada2ecdbf0b98cc5120d86d21313d6640a00b
JasonYCHuang <jasonych99@gmail.com>
2021-11-03 09:43:51 +0800

    add visx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,14 @@
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import Counter from './counter';
+import ZoomI from './d3_zoom';
+import BrushChart from './d3_brush';
 
 const EntryPoint = () => {
   return (
     <div>
       <Counter />
+      <ZoomI width={400} height={300} />
+      <BrushChart width={800} height={600} />
     </div>
   );
 };
