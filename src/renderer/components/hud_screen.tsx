e00ecaff5123b64103d48cbc579d6d643d6ae998
JasonYCHuang <jasonych99@gmail.com>
2021-11-07 15:27:33 +0800

    upd

--- src/renderer/components/hud_screen.tsx
+++ src/renderer/components/hud_screen.tsx
@@ -1,39 +1,61 @@
-import { useAppDispatch, useAppSelector } from '../reducers/hooks';
-import { writeSelectedImageDim } from '../actions/screenSlice';
-import type { AppDispatch } from '../reducers/store';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-interface HudVisxProps {
+const componentW = 600;
+
+interface HudScreenProps {
   file: File;
+}
+
+interface TypeImgSt {
+  loaded: boolean;
+  imgW: number;
+  imgH: number;
+  ratio: number;
 }
 
 const selectedSvgStyle = {
   border: '1px solid blue',
 };
 
-const setImg = (file: File, dispatch: AppDispatch) => {
+const loadImg = (file: File, setImgSt: Dispatch<SetStateAction<TypeImgSt>>) => {
   const img = new Image();
   img.onload = () => {
-    dispatch(
-      writeSelectedImageDim({
-        imgW: img.width,
-        imgH: img.height,
-      })
-    );
+    setImgSt({
+      loaded: true,
+      imgW: img.width,
+      imgH: img.height,
+      ratio: img.width / componentW,
+    });
   };
   img.src = URL.createObjectURL(file);
   return img;
 };
 
-const HudScreen = ({ file }: HudVisxProps) => {
-  const { imgW } = useAppSelector((state: any) => state.screen);
-  console.log(imgW)
-  const dispatch = useAppDispatch();
-
-  const img = setImg(file, dispatch);
+const HudScreen = ({ file }: HudScreenProps) => {
+  const [imgSt, setImgSt] = useState<TypeImgSt>({
+    loaded: false,
+    imgW: 0,
+    imgH: 0,
+    ratio: 0,
+  });
+  const img = loadImg(file, setImgSt);
+  const height = imgSt.loaded ? Math.round(imgSt.imgH / imgSt.ratio) : 200;
 
   return (
-    <svg id="selected-svg" width={600} style={selectedSvgStyle}>
-      <image id="selected-image" x="0" y="0" width={imgW} xlinkHref={img.src} />
+    <svg
+      id="selected-svg"
+      width={componentW}
+      height={height}
+      style={selectedSvgStyle}
+    >
+      <image
+        id="selected-image"
+        x="0"
+        y="0"
+        width={componentW}
+        xlinkHref={img.src}
+        style={{ display: imgSt.loaded ? 'block' : 'none' }}
+      />
     </svg>
   );
 };
